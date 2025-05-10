8fc1b192a4bc601fc16666fd15e262c41a08c370
enkhbold470 <enkhbold470@gmail.com>
2025-05-10 04:27:28 -0700

    a

--- gavel/utils.py
+++ gavel/utils.py
@@ -2,8 +2,7 @@ from gavel import celery
 import gavel.settings as settings
 import gavel.crowd_bt as crowd_bt
 import gavel.constants as constants
-from flask import Response, request, render_template
-from markupsafe import Markup
+from flask import Markup, Response, request, render_template
 import markdown
 import requests
 from functools import wraps
